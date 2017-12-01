763189898d7dd75c1574837622259ea97f98d926
Fuzzbawls <fuzzbawls@gmail.com>
2017-11-30 21:49:49 -0800

    Squashed 'src/univalue/' content from commit dac5296
    git-subtree-dir: src/univalue
    git-subtree-split: dac529675973c5da1e32414e1274908836399d9b

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+<<<<<<< HEAD
 # Copyright (c) 2013-2016 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+=======
+>>>>>>> ef1718b79... Squashed 'src/univalue/' content from commit dac5296
 set -e
 srcdir="$(dirname $0)"
 cd "$srcdir"
@@ -10,6 +13,10 @@ if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
   LIBTOOLIZE="${GLIBTOOLIZE}"
   export LIBTOOLIZE
 fi
+<<<<<<< HEAD
 which autoreconf >/dev/null || \
   (echo "configuration failed, please install autoconf first" && exit 1)
 autoreconf --install --force --warnings=all
+=======
+autoreconf --install --force
+>>>>>>> ef1718b79... Squashed 'src/univalue/' content from commit dac5296
